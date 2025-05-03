9e41e3b91ea1f69c97485952e3543cf2af325b2c
manojsingh-manoj <manojsingh.manoj@gmail.com>
2025-05-03 17:18:54 +0200

    Add sort with date, city

--- src/main/java/com/klm/weather/repository/WeatherRepository.java
+++ src/main/java/com/klm/weather/repository/WeatherRepository.java
@@ -1,9 +1,25 @@
 package com.klm.weather.repository;
 
 import com.klm.weather.model.Weather;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface WeatherRepository extends JpaRepository<Weather, Integer> {
+
+    @Query("SELECT w FROM Weather w WHERE LOWER(w.city) IN :cities")
+    List<Weather> findByCityInIgnoreCase(@Param("cities") List<String> citiesLower, Sort sort);
+
+    List<Weather> findByDate(Sort sortCriteria, Date date);
+    @Query("SELECT w FROM Weather w WHERE LOWER(w.city) IN :cities AND w.date between :dateBefore AND :dateAfter")
+    List<Weather> findByDateAndCityIn(Sort sortCriteria, Date dateBefore, Date dateAfter, List<String> city);
+
+    List<Weather> findByDateBetween(Date dateBefore, Date dateAfter, Sort sort);
+
 }
--- src/main/java/com/klm/weather/service/WeatherService.java
+++ src/main/java/com/klm/weather/service/WeatherService.java
@@ -1,5 +1,6 @@
 package com.klm.weather.service;
 
+import com.klm.weather.exception.InvalidDateFormatException;
 import com.klm.weather.exception.WeatherIdNotFoundException;
 import com.klm.weather.model.Weather;
 import com.klm.weather.repository.WeatherRepository;
@@ -8,6 +9,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -20,7 +24,36 @@ public class WeatherService {
 
     public List<Weather> getWeather(String sort, String date, String city) {
         Sort sortCriteria = Sort.by(Sort.Direction.ASC, "id");
+        if (sort != null) {
+            sortCriteria = sort.startsWith("-") ? Sort.by(Sort.Direction.DESC, sort.substring(1)) : Sort.by(Sort.Direction.ASC, sort);
+        }
+        if (date != null && city != null) {
+            Date ldate =  getParsedDate(date);
+            return weatherRepository.findByDateAndCityIn(sortCriteria, getParsedDate(date), getEndDate(ldate),List.of(city.toLowerCase().split(",")));
+        } else if (date != null) {
+            Date ldate =  getParsedDate(date);
+            return weatherRepository.findByDateBetween(ldate,getEndDate(ldate),sortCriteria);
+            //return weatherRepository.findByDate(sortCriteria, getParsedDate(date));
+        } else if (city != null) {
+            return weatherRepository.findByCityInIgnoreCase(List.of(city.toLowerCase().split(",")),sortCriteria);
+        }
+
         return weatherRepository.findAll(sortCriteria);
+    }
+
+    private static Date getEndDate(Date ldate) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(ldate);
+        c.add(Calendar.DAY_OF_MONTH, 1);
+        return c.getTime();
+    }
+
+    private static Date getParsedDate(String date) {
+        try {
+            return Constant.DATE_FORMAT.parse(date);
+        } catch (ParseException e) {
+            throw new InvalidDateFormatException(Constant.STR_INVALID_DATE_FORMAT);
+        }
     }
 
     public Weather getWeatherById(Integer id) {
